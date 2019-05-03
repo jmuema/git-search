10397cdb7e51c9eb08ee05f816af5f4878c9f6c9
josephmuema <jmuema95@gmail.com>
2019-05-03 12:00:13 +0300

    imported httpclientmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileFormComponent } from './profile-form/profile-form.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfileService } from './profiles/profile.service';
+const routes: Routes = [
+  {path: 'profile', component: ProfileComponent},
+  {path: 'repositories', component: RepositoriesComponent}
+];
 
 @NgModule({
   declarations: [
@@ -16,9 +26,12 @@ import { RepositoriesComponent } from './repositories/repositories.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
