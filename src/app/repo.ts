faa5e77633b56b56a12eb94dc496e118eff6ab94
josephmuema <jmuema95@gmail.com>
2019-05-05 23:24:31 +0300

    Auto-generated commit

--- src/app/repo.ts
+++
@@ -1,3 +0,0 @@
-export class Repo {
-  constructor(public name: string, public description: string, public html_url: string, public  clone_url: string, public homepage:string ) {}
-  }
--- src/app/searching .ts
+++
@@ -1,3 +0,0 @@
-export class Searching {
-  constructor(public name: string) {}
-} 
